72dd38a8e0a835f3c82dcb0dc30b6dd0bb9bfc55
AkramSilawat <akram.khan.silawat.aks@gmail.com>
2024-02-10 13:41:41 +0530

    Object literals

---
+++ 01-object literals.js
@@ -0,0 +1,12 @@
+var userOne = {
+    email: 'ryu@ninjas.com',
+    name: 'ryu',
+    login() {
+        console.log(this.email, 'has logged in');
+    },
+    logout(){
+        console.log(this.email, 'has logged out');
+    }
+};
+
+console.log(userOne.name);
